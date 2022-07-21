ceed6bc77c26ac6053c7aacd59bffcd15d27f5d5
Justin Reeves <reeves122@gmail.com>
2022-07-21 11:51:14 -0500

    update terraform-module-template ref

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 module "this" {
-  source = "github.com/champ-oss/terraform-module-template.git?ref=v1.0.18-a5e38c8"
+  source = "github.com/champ-oss/terraform-module-template.git?ref=85ab230c4f873f7a34e068ec7630cfd1197bdf8d"
 }
